8c411fc7c295def4a167049bc164d25fa9a8472a
sorikiki <sorikiki98@naver.com>
2020-09-05 12:17:17 +0900

    tuples

--- Learn Python/4. List.py
+++ Learn Python/4. List.py
@@ -26,7 +26,7 @@ my_list = [1, 2, 3]
 my_list + [4] # => this is valid.
 
 
-# ✅ Range
+# ✅ Range (❗ variable's name should not be 'range')
 # : Often, we want to create a list of consecutive numbers. 
 # => The function 'range' takes a single input, and generates numbers starting at 0 and ending at the number before the input by default.
 # => ex. So, if we want the numbers from 0 through 9, we use range(10) because 10 is 1 greater than 9
@@ -54,7 +54,7 @@ print(len(my_list))
 # We can use the index -1 to select the last item of a list, even when we don’t know how many elements are in a list.
 
 
-# ✅ Slicing Lists(= this is same with javascript's built-in array method 'slice')
+# ✅ Slicing Lists
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 letters[0:3] # ['a','b','c']
 
@@ -68,6 +68,11 @@ print(fruits[2:]) # ['cherry' , 'date']
 # - If we want to select the last 3 elements of fruits, we can also use this syntax:
 print(fruits[-3:]) # ['banana', 'cherry', 'date']
 
+# - The original array is not changed.
+def remove_middle(lst, start, end):
+  return lst[:start] + lst[end+1:]
+
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3)) # [4, 23, 42]
 
 # ✅ Counting elements
 letters = ['m', 'i', 's', 's', 'i', 's', 's', 'i', 'p', 'p', 'i']
@@ -90,6 +95,51 @@ sorted_names = sorted(names)
 print(sorted_names) # ['Angel', 'Buffy', 'Giles', 'Willow', 'Xander'] => alphabetically
 
 
+# ✅ What are Tuples?
+# : a data structure to store multiple pieces of data
+# ✔ differences from Lists
+# => immutable: do not add, remove or change
+# => are always enclosed by ()
+# => usually contain a heterogeneous sequence of elements that are accessed via unpacking or indexing.(negative indexing also available.)
+# cf. Lists are mutable, and their elements are usually homogeneous and are accessed by iterating over the list.
 
+# ✔ other properties
+# => immutable, but there is a workaround ↪: tuple => list => tuple
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x) # (apple, kiwi, cherry)
 
+# ✔ warning: A special problem is the construction of tuples containing 0 or 1 items
+# ◽ Empty tuples are constructed by an empty pair of parentheses
+# => If it is not an empty tuple, it is not necessary to use parentheses.
+empty = ()
+print(len(empty)) # 0
+# ◽ a tuple with one item is constructed by following a value with a comma (it is not sufficient to enclose a single value in parentheses)
+singleton = 'hello', # trailing comma is needed.
+print(len(singleton)) # 1
+print(singleton) # ('hello',)
 
+# ✔ unpacking: It is to release a number of tied values.
+'''
+>>> my_tuple = 1, 2, 3
+>>> 
+>>> num1, num2, num3 = my_tuple
+>>> 
+>>> num1
+1
+>>> num2
+2
+>>> num3
+3
+'''
+# => If you want to change num1 = 2, num = 1
+'''
+>>> num1, num2 = num2, num1
+>>> #left: unpacking, right: packing 된 것
+>>> num1
+2
+>>> num2
+1
+'''
