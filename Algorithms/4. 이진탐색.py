0f957f53dffc9c2f1323b978c5a53f0a4d964187
sorikiki <sorikiki98@naver.com>
2021-01-25 18:10:20 +0900

    떡볶이 떡 자르기

--- Algorithms/4. 이진탐색.py
+++ Algorithms/4. 이진탐색.py
@@ -131,3 +131,48 @@ for target in ordered_product:
     print("yes", end=" ")
   else:
     print("no", end=" ") 
+
+# ✅ 떡볶이 떡 만들기
+n, m = map(int, input().split())
+temp = list(map(int, input().split()))
+
+def sliceCake(start, end):
+  while True:
+    # 중간 인덱스와 잘린 떡 길이 구하기
+    mid = (start+end)// 2
+    h = 0
+    for i in temp:
+      if i>mid:
+        h += (i-mid)
+    # 반복문 중단 조건
+    if h == m or end-start == 1:
+      return mid
+    elif h > m:
+      start = mid
+    else:
+      end = mid
+
+
+print(sliceCake(0, max(temp)))
+
+# ◽ 중간값(mid)과는 별개로 새로운 변수 만들어 답안 작성
+n, m = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+
+result = 0
+while start <= end:
+  total = 0
+  mid = (start + end) // 2
+  for x in array:
+    if x > mid:
+      total += x - mid
+  # 떡의 양이 부족한 경우 더 많이 자르기
+  if total < m:
+    end = mid-1
+  # 떡의 양이 충분한 경우 덜 자르기
+  else:
+    result = mid # 최대한 덜 잘랐을 때가 정답이므로, 여기에서 result를 기록
+    start = mid + 1
