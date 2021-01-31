12e89831b65969a08051c14267dfad1c4ff9ba95
sorikiki <sorikiki98@naver.com>
2021-01-31 23:32:50 +0900

    바닥 공사

--- Algorithms/5. 다이나믹 프로그래밍.py
+++ Algorithms/5. 다이나믹 프로그래밍.py
@@ -93,3 +93,13 @@ d[1] = max(array[0], array[1])
 for i in range(2, n):
   d[i] = max(d[i-1], d[i-2] + array[i])
 print(d[n-1])
+
+# ✅ 바닥 공사
+n = int(input())
+d = [0] * 1001
+d[1] = 1
+d[2] = 3
+for i in range(3, n+1):
+  d[i] = (d[i-1] + 2 * d[i-2]) % 796796
+
+print(d[n])
